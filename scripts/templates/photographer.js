fa363a55c73db8d585ac2a625180366e9c98e7db
David <davidroussel171113@outlook.fr>
2023-11-25 15:55:09 +0100

    MAJ getUserCardDOM()

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,32 @@
-function photographerTemplate(data) {
-    const { name, portrait } = data;
+async function photographerTemplate(data) {
+    const reponce = await fetch("./data/photographers.json");
+    data = await reponce.json();
+    console.log(data[0]);
 
-    const picture = `assets/photographers/${portrait}`;
-
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+    async function getUserCardDOM() {
+        const article = document.createElement('article');
+        const idPhotographer = data.id;
+        const imgPhotographer = document.createElement('img');
+        imgPhotographer.setAttribute("src", data.portrait)
+        const namePhotographer = document.createElement('h2');
+        namePhotographer.textContent = data.name;
+        const countryPhotographer = document.createElement("p");
+        countryPhotographer.textContent = data.country;
+        const cityPhotographer = document.createElement("p");
+        cityPhotographer.textContent = data.city;
+        const tagLinePhotographer = document.createElement("p");
+        tagLinePhotographer.textContent = data.tagline;
+        const pricePhotographer = document.createElement("p");
+        pricePhotographer.textContent = `${data.price}€/jour`;
+        article.appendChild(idPhotographer);
+        article.appendChild(imgPhotographer);
+        article.appendChild(namePhotographer);
+        article.appendChild(countryPhotographer);
+        article.appendChild(cityPhotographer);
+        article.appendChild(tagLinePhotographer);
+        article.appendChild(pricePhotographer);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+
+    return {getUserCardDOM}
 }
