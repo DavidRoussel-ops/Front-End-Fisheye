e98ecf0919eafde55f3ddf4451a0dba9dbf8f14b
David <davidroussel171113@outlook.fr>
2023-11-27 00:13:13 +0100

    Add fetch call to file json

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,22 +1,19 @@
 async function getPhotographers() {
-    // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
-    // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
     const reponce = await fetch("./data/photographers.json");
-    const photographers = await reponce.json();
-    console.log(photographers);
-    console.log(photographerTemplate());
-    // et bien retourner le tableau photographers seulement une fois récupéré
-    return photographers
+    const reponceContenu = await reponce.json();
+    console.log(reponceContenu);
+    return reponceContenu
 }
 
 async function displayData(photographers) {
     const photographersSection = document.querySelector(".photographer_section");
 
-    for (const photographer of photographers) {
+    photographers.forEach((photographer) => {
         const photographerModel = photographerTemplate(photographer);
         const userCardDOM = photographerModel.getUserCardDOM();
-        await photographersSection.appendChild(userCardDOM);
-    };
+        console.log(userCardDOM);
+        photographersSection.appendChild(userCardDOM);
+    });
 }
 
 async function init() {
@@ -25,5 +22,5 @@ async function init() {
     await displayData(photographers);
 }
 
-init().then();
+init();
     
