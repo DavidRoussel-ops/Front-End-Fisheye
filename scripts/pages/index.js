75787b9980318eeb0ac5edce8e99af3f11788a8c
David <davidroussel171113@outlook.fr>
2023-12-11 23:58:46 +0100

    Modification

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -3,26 +3,28 @@ import {photographerTemplate} from "../templates/photographer.js";
 async function getPhotographers() {
     const reponce = await fetch("http://localhost:8081/photographers");
     const reponceContenu = await reponce.json();
-    console.log(reponceContenu);
     return reponceContenu
 }
 
 async function displayData() {
     let contenu = await fetch("http://localhost:8081/photographers");
     let photographers = await contenu.json();
-    let prout = getPhotographers();
-    console.log(prout);
 
     for await (let photographer of photographers) {
         const photographerModel = photographerTemplate(photographer);
         const userCardDOM = (await photographerModel).getUserCardDOM(photographer);
     }
-
-
-    /*photographers.forEach((photographer) => {
-
-    })*/
 }
+
+async function getIdPhotographers() {
+    const response = await fetch("http://localhost:8081/photographers");
+    const responsePhotographer = await response.json();
+    console.log(responsePhotographer);
+    const idPhotographer = responsePhotographer.map(photographer => photographer.id);
+    console.log(idPhotographer);
+}
+
+getIdPhotographers();
 
 async function init() {
     // Récupère les datas des photographes
