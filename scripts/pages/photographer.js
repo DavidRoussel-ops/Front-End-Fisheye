e46b1cf4d8f50022eee6816fca5e30249042f0b0
David <davidroussel171113@outlook.fr>
2023-12-13 00:00:11 +0100

    Update display function photographer personal page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,19 +1,21 @@
-//Mettre le code JavaScript lié à la page photographer.html
+import {photographerIdTemplate} from "../templates/photographerId.js";
+
 async function getMedia() {
-    const response = await fetch("http://localhost:63342/Front-End-Fisheye/data/photographers.json");
+    const response = await fetch("http://localhost:8081/photographers");
     const responseMedia = await response.json();
     console.log(responseMedia);
     return responseMedia;
 }
 
-async function displayPhotographers(photographers) {
-    const photographersSection = document.querySelector(".photographer-header");
+async function displayPhotographers() {
+    const apiPhotographer = await fetch("http://localhost:8081/photographers");
+    const arrayPhotographers = await apiPhotographer.json();
 
-    photographers.forEach((photographer) => {
-        const photographerModel = photographerTemplate(photographer);
-        const userCardDOM = photographerModel.getUserCardDOM();
-        photographersSection.appendChild(userCardDOM);
-    });
+    for await (let photographer of arrayPhotographers) {
+        const photographerIdModel = photographerIdTemplate(photographer);
+        const idCardDOM = (await photographerIdModel).getOnePhotographer(photographer);
+        return idCardDOM
+    }
 }
 
 async function init() {
