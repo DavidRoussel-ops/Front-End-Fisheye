e6d63a4a8bf8293ee6994d2bcd87b8dc695ca114
David <davidroussel171113@outlook.fr>
2023-12-15 10:02:38 +0100

    setting up media

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,11 +1,18 @@
 import {photographerIdTemplate} from "../templates/photographerId.js";
 import {createModal} from "../templates/modal.js";
+import {mediaTemplate} from "../templates/media.js";
 
 async function getPhotographers() {
     const response = await fetch("http://localhost:8081/photographers");
-    const responseMedia = await response.json();
-    console.log(responseMedia);
-    return responseMedia;
+    const responsePhotographer = await response.json();
+    console.log(responsePhotographer);
+    return responsePhotographer;
+}
+
+async function getMedias() {
+    const fetchCall = await fetch("http://localhost:8081/media");
+    const responceMedia = await fetchCall.json();
+    console.log(responceMedia);
 }
 
 async function displayPhotographers() {
@@ -13,9 +20,28 @@ async function displayPhotographers() {
     const arrayPhotographers = await apiPhotographer.json();
 
     for await (let photographer of arrayPhotographers) {
+        console.log(photographer);
         const photographerIdModel = photographerIdTemplate(photographer);
         const idCardDOM = (await photographerIdModel).getOnePhotographer(photographer);
         return idCardDOM
+    }
+}
+
+async function displayMedias(data) {
+    const apiMedia = await fetch("http://localhost:8081/media");
+    const arrayMedias = await apiMedia.json();
+    console.log(arrayMedias);
+
+    for (let i = 0; i < arrayMedias.length; i++) {
+        let idPhotographer = arrayMedias.map(arrayMedias => arrayMedias.photographerId = 243);
+        console.log(idPhotographer);
+    }
+
+    for (let media of arrayMedias) {
+        const mediaModel = mediaTemplate(media);
+        const mediaDOM = (await mediaModel).getMediaResult(media);
+        console.log(mediaDOM);
+        return mediaDOM
     }
 }
 
@@ -29,6 +55,8 @@ async function showModal() {
 async function init() {
     const { photographers } = await getPhotographers();
     await displayPhotographers(photographers);
+    const { medias } = await getMedias() || {};
+    await displayMedias(medias);
     await showModal();
 }
 
