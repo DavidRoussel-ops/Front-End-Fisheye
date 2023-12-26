01cad89911123339f01c9f3a549548c536d9901c
David <davidroussel171113@outlook.fr>
2023-12-26 11:44:30 +0100

    creation function to past the photographer's id to localStorage

--- scripts/utils/changePage.js
+++ scripts/utils/changePage.js
@@ -1,19 +1,8 @@
-export function getIdPhotographer() {
-    const linkPhotographer = document.querySelectorAll(".containerPictureH2");
-
-    for (let i = 0; i < linkPhotographer.length; i++) {
-
-        linkPhotographer[i].addEventListener("click", async function (event) {
-
-            const id = event.target.dataset.id;
-            const rep = await fetch(`http://localhost:8081/photographers/${id}`);
-            const jsonRep = await rep.json();
-            const parentElement = event.target.parentElement;
-
-            const goPhotographer = document.createElement("a");
-            goPhotographer.setAttribute("href", `${id}/photographer.html`);
-            console.log(goPhotographer);
-            parentElement.appendChild(goPhotographer);
-        })
-    }
+export function postIdPhotographer() {
+    const linkPhotographer = document.querySelector(".photographer_section");
+    linkPhotographer.addEventListener("click", function (event) {
+        console.log(event.target.dataset.id);
+        const id = event.target.dataset.id;
+        window.localStorage.setItem("id", id);
+    });
 }
