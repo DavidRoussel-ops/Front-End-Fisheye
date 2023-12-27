7ecfa09a0bd00f33b9306485f4c1e76f4777db11
David <davidroussel171113@outlook.fr>
2023-12-28 00:24:16 +0100

    Modification of the function in order to be able to save the datas in localStorage

--- scripts/utils/changePage.js
+++ scripts/utils/changePage.js
@@ -1,8 +1,19 @@
 export function postIdPhotographer() {
     const linkPhotographer = document.querySelector(".photographer_section");
     linkPhotographer.addEventListener("click", function (event) {
-        console.log(event.target.dataset.id);
-        const id = event.target.dataset.id;
+        let id = event.target.querySelector(".pId").textContent;
+        let img = event.target.querySelector("img").src;
+        let name = event.target.querySelector("h2").textContent;
+        let city = event.target.querySelector(".pCity").textContent;
+        let country = event.target.querySelector(".pCountry").textContent;
+        let tagline = event.target.querySelector(".pTagline").textContent;
+        let price = event.target.querySelector(".pPrice").textContent;
         window.localStorage.setItem("id", id);
+        window.localStorage.setItem("picture", img);
+        window.localStorage.setItem("name", name);
+        window.localStorage.setItem("city", city);
+        window.localStorage.setItem("country", country);
+        window.localStorage.setItem("tagline", tagline);
+        window.localStorage.setItem("price", price);
     });
 }
