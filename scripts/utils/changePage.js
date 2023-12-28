e5191a1ed11dbb7b263068e22f3a9d6bb046e45d
David <davidroussel171113@outlook.fr>
2023-12-28 23:16:03 +0100

    Modification for send object on localStorage

--- scripts/utils/changePage.js
+++ scripts/utils/changePage.js
@@ -8,12 +8,15 @@ export function postIdPhotographer() {
         let country = event.target.querySelector(".pCountry").textContent;
         let tagline = event.target.querySelector(".pTagline").textContent;
         let price = event.target.querySelector(".pPrice").textContent;
-        window.localStorage.setItem("id", id);
-        window.localStorage.setItem("picture", img);
-        window.localStorage.setItem("name", name);
-        window.localStorage.setItem("city", city);
-        window.localStorage.setItem("country", country);
-        window.localStorage.setItem("tagline", tagline);
-        window.localStorage.setItem("price", price);
+        let photographerInfo = {
+            "id" : id,
+            "picture" : img,
+            "name" : name,
+            "city" : city,
+            "country" : country,
+            "tagline" : tagline,
+            "price" : price
+        };
+        window.localStorage.setItem("photographe", JSON.stringify(photographerInfo));
     });
 }
