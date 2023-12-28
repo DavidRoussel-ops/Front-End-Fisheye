9967fef6ecb8f94e0759452145cde088077c970c
David <davidroussel171113@outlook.fr>
2023-12-28 23:54:24 +0100

    Add loop for sending data to localStorage

--- scripts/utils/changePage.js
+++ scripts/utils/changePage.js
@@ -1,22 +1,25 @@
 export function postIdPhotographer() {
-    const linkPhotographer = document.querySelector(".photographer_section");
-    linkPhotographer.addEventListener("click", function (event) {
-        let id = event.target.querySelector(".pId").textContent;
-        let img = event.target.querySelector("img").src;
-        let name = event.target.querySelector("h2").textContent;
-        let city = event.target.querySelector(".pCity").textContent;
-        let country = event.target.querySelector(".pCountry").textContent;
-        let tagline = event.target.querySelector(".pTagline").textContent;
-        let price = event.target.querySelector(".pPrice").textContent;
-        let photographerInfo = {
-            "id" : id,
-            "picture" : img,
-            "name" : name,
-            "city" : city,
-            "country" : country,
-            "tagline" : tagline,
-            "price" : price
-        };
-        window.localStorage.setItem("photographe", JSON.stringify(photographerInfo));
-    });
+    const linkPhotographer = document.querySelectorAll(".containerPictureH2");
+    for (let i = 0; i < linkPhotographer.length; i++) {
+        linkPhotographer[i].addEventListener("click", function (event) {
+            let id = event.target.querySelector(".pId").textContent;
+            let img = event.target.querySelector("img").src;
+            let name = event.target.querySelector("h2").textContent;
+            let city = event.target.querySelector(".cityInfo").textContent;
+            let country = event.target.querySelector(".countryInfo").textContent;
+            let tagline = event.target.querySelector(".taglineInfo").textContent;
+            let price = event.target.querySelector(".priceInfo").textContent;
+            let photographerInfo = {
+                "id" : id,
+                "picture" : img,
+                "name" : name,
+                "city" : city,
+                "country" : country,
+                "tagline" : tagline,
+                "price" : price
+            };
+            window.localStorage.setItem("photographe", JSON.stringify(photographerInfo));
+        });
+    }
+
 }
