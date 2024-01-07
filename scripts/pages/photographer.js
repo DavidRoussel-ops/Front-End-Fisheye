d558b3f3ccbb93bb8a7551032abb0f7ed7cad0e0
David <davidroussel171113@outlook.fr>
2024-01-08 00:08:00 +0100

    Implementation of functions for calling the media API and displaying the media corresponding to the selected photographer.

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,33 +1,31 @@
 import {photographerIdTemplate} from "../templates/photographerId.js";
-//import {createModal} from "../templates/modal.js";
 import {mediaTemplate} from "../templates/media.js";
 
-let params = new URLSearchParams(document.location.search)
-let idPage = parseInt(params.get("id"));
-console.log(idPage);
 
-/*async function getMedias() {
-    const media = await fetch("http://localhost:8081/media").then(media => media.json());
-    let mediaTemplate = mediaTemplate();
-    let afficheMedia = (await mediaTemplate).getMediaResult(media);
+async function showMedias() {
+    const medias = await fetch("http://localhost:8081/media").then(media => media.json());
+    return medias;
 }
 
-async function displayMedias(media) {
+async function displayMedias() {
 
+    const medias = await fetch("http://localhost:8081/media").then(medias => medias.json());
+    for (let media of medias) {
+        let params = new URLSearchParams(document.location.search)
+        let idPage = parseInt(params.get("id"));
+        let template = mediaTemplate(media);
+        let templatePhotographerId = (await template).photographerId;
+        console.log(templatePhotographerId);
+        if (idPage === templatePhotographerId) {
+            let afficherMedias = (await template).getMediaResult();
+        }
+    }
 }
 
-async function showModal() {
-    const modalContact = document.getElementById("contact_modal");
-    const base = createModal();
-    const modal = (await base).modal();
-    await modalContact.appendChild(modal);
-}
-*/
 async function init() {
     photographerIdTemplate();
-    //const { medias } = await getMedias() || {};
-    //await displayMedias(medias);
-    //await showModal();
+    const { medias } = await showMedias();
+    await displayMedias(medias);
 }
 
 init();
