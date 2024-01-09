787290f0c092dcd5b9c66a938de057bce21148ed
David <davidroussel171113@outlook.fr>
2024-01-09 17:58:11 +0100

    Finished function showTotalLikes and draft function to display the lightbox template.

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,6 +1,6 @@
 import {photographerIdTemplate} from "../templates/photographerId.js";
 import {mediaTemplate} from "../templates/media.js";
-
+import {lightBoxTemplate} from "../templates/lightBox.js";
 
 async function showMedias() {
     const medias = await fetch("http://localhost:8081/media").then(media => media.json());
@@ -15,27 +15,54 @@ async function displayMedias() {
         let idPage = parseInt(params.get("id"));
         let template = mediaTemplate(media);
         let templatePhotographerId = (await template).photographerId;
-        console.log(templatePhotographerId);
         if (idPage === templatePhotographerId) {
-            let afficherMedias = (await template).getMediaResult();
+            await (await template).getMediaResult();
         }
     }
 }
 
-async function showTotalLikes() {
-    let likes = document.querySelectorAll(".likeMedia").innerText;
-    for (let i = 0; i < likes.length; i++) {
-        let arrayLikes = [likes[i].innerText];
-        console.log(arrayLikes);
-        console.log(likes[i].innerText);
+async function displayLightbox() {
+
+    const medias = await fetch("http://localhost:8081/media").then(medias => medias.json());
+    let params = new URLSearchParams(document.location.search);
+    let idPage = parseInt(params.get("id"));
+    let template = lightBoxTemplate(medias);
+    let templatePhotographerId = (await template).photographerId;
+    if (idPage === templatePhotographerId) {
+        await (await template).getMediaLightBox();
     }
-    console.log(likes);
+}
+
+async function showTotalLikes() {
+    const likesPrices = document.querySelector(".likesPrices");
+    let likes = document.querySelectorAll(".likeMedia");
+    let arrayLikes = [];
+    for (let i = 0; i < likes.length; i++) {
+        arrayLikes.push(parseInt(likes[i].innerText));
+    }
+    let sum = 0;
+    for (let i = 0; i < arrayLikes.length; i++) {
+        sum += arrayLikes[i];
+    }
+    const divTotalLikes = document.createElement("div");
+    divTotalLikes.classList.add("divTotalLikes");
+    const total = document.createElement("p");
+    total.textContent = sum;
+    total.classList.add("total");
+    const heartTotal = document.createElement("img");
+    heartTotal.setAttribute("src", "https://img.icons8.com/windows/32/filled-heart.png");
+    heartTotal.setAttribute("alt", "coeur");
+    heartTotal.classList.add("heartTotal");
+    divTotalLikes.appendChild(total);
+    divTotalLikes.appendChild(heartTotal);
+    likesPrices.appendChild(divTotalLikes);
 }
 
 async function init() {
     photographerIdTemplate();
     const { medias } = await showMedias();
     await displayMedias(medias);
+    await displayLightbox(medias);
     await showTotalLikes();
 }
 
