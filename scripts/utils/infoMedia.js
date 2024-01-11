62b374496cbd5c22898959e116d11c40e7c17cec
David <davidroussel171113@outlook.fr>
2024-01-11 15:47:49 +0100

    Draft function to send media to localStorage.

---
+++ scripts/utils/infoMedia.js
@@ -0,0 +1,17 @@
+export function postInfoMedia() {
+    const linkMedia = document.querySelectorAll(".linkLightbox");
+    for (let i = 0; i < linkMedia.length; i++) {
+        linkMedia[i].addEventListener("click", function (event) {
+            console.log(event.target);
+            let image = event.target.querySelector(".imageMedia").src;
+            let video = event.target.querySelector(".playMedia").src;
+            let title = event.target.querySelector(".titleMedia").textContent;
+            let infoMedia = {
+                "picture" : image,
+                "video" : video,
+                "title" : title
+            };
+            window.localStorage.setItem("media", JSON.stringify(infoMedia));
+        });
+    }
+}
